467e306cff81cce6b8a1ba93c3b810e643579da9
rahul0218 <kumar.rahul0218@gmail.com>
2022-08-09 10:13:44 +0530

    final

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Dishariinstituteofnursingsciencemalda/';
-//$config['base_url'] = 'http://dipaskolkata.com/';
+$config['base_url'] = 'http://localhost/dinsmalda/';
+//$config['base_url'] = 'http://dinsmalda.com/';
 
 /*
 |--------------------------------------------------------------------------
